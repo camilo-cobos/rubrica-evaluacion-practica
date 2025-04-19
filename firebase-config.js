e6a067e229e770e6979ccfcd8431a02ba2125393
camilo-cobos <jccobosc@udistrital.edu.co>
2025-04-19 13:31:01 -0500

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA9-4s39ruk896JqWJYgefouu_i9-2lGQE",
+  authDomain: "rubrica-matematicas.firebaseapp.com",
+  projectId: "rubrica-matematicas",
+  storageBucket: "rubrica-matematicas.firebasestorage.app",
+  messagingSenderId: "180161782933",
+  appId: "1:180161782933:web:e65c66fb9e5411b67be79c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
