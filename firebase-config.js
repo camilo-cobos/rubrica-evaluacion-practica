da7fddf407298952884e9a5cf0f6e09a3fd3fc67
camilo-cobos <jccobosc@udistrital.edu.co>
2025-04-19 14:06:57 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,3 +1,9 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyA9-4s39ruk896JqWJYgefouu_i9-2lGQE",
   authDomain: "rubrica-matematicas.firebaseapp.com",
