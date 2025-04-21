8fc556395f3d377e478f3222bd66ffbfe588ec78
camilo-cobos <jccobosc@udistrital.edu.co>
2025-04-20 22:55:23 -0500

    Update firebase-setup.js

--- firebase-setup.js
+++ firebase-setup.js
@@ -1,7 +1,19 @@
 // firebase-setup.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-app.js";
-import { getFirestore, doc, getDoc, setDoc } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-firestore.js";
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc,
+  collection,
+  addDoc,
+  getDocs,
+  query,
+  orderBy,
+  serverTimestamp
+} from "https://www.gstatic.com/firebasejs/10.10.0/firebase-firestore.js";
 
+// Configuración de Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyDqbbvsipR4zxsk4VPq3k_VbePqpsjD9Hc",
   authDomain: "rubrica-evaluacion.firebaseapp.com",
@@ -11,9 +23,21 @@ const firebaseConfig = {
   appId: "1:78519129707:web:c25124bcbc6e85b6cbc32e"
 };
 
-// Inicializa Firebase y Firestore
+// Inicializa Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-// Exporta la base de datos
-export { db, doc, getDoc, setDoc };
+// Exportar todo lo necesario
+export {
+  db,
+  doc,
+  getDoc,
+  setDoc,
+  collection,
+  addDoc,
+  getDocs,
+  query,
+  orderBy,
+  serverTimestamp
+};
+
